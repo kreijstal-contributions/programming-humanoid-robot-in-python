fe64e4196392619eb0ff15987345176b17dee02f
kaka012 <kaka012@users.noreply.github.com>
2021-11-03 13:51:38 +0100

    Solve exercise 1

--- introduction/get_sensor_data.py
+++ introduction/get_sensor_data.py
@@ -3,7 +3,7 @@ In this exercise you need to know how to get sensor data.
 
 * Task: get the current joint angle and temperature of joint HeadYaw
 
-* Hint: The current sensor data of robot are store in perception (class Perception in spark_agent.py)
+* Hint: The current sensor data of robot is stored in perception (class Perception in spark_agent.py)
 
 '''
 
@@ -17,12 +17,10 @@ from spark_agent import SparkAgent
 
 class MyAgent(SparkAgent):
     def think(self, perception):
-        angle = 0
-        temperature = 0
-        # YOUR CODE HERE
-        # get angle and temperature to current data of joint HeadYaw
+        angle = perception.joint["HeadYaw"]
+        temperature = perception.joint_temperature["HeadYaw"]
 
-        print('HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature))
+        print(f'HeadYaw angle: {angle} temperature: {temperature}')
         return super(MyAgent, self).think(perception)
 
 if '__main__' == __name__:
--- introduction/set_joint_commands.py
+++ introduction/set_joint_commands.py
@@ -5,7 +5,7 @@ In this exercise you need to know how to set joint commands.
     1. set stiffness of LShoulderPitch to 0
     2. set speed of HeadYaw to 0.1
 
-* Hint: The commands are store in action (class Action in spark_agent.py)
+* Hint: The commands are stored in action (class Action in spark_agent.py)
 
 '''
 
@@ -20,7 +20,8 @@ from spark_agent import SparkAgent
 class MyAgent(SparkAgent):
     def think(self, perception):
         action = super(MyAgent, self).think(perception)
-        # YOUR CODE HERE
+        action.stiffness['LShoulderPitch'] = 0
+        action.speed['HeadYaw'] = 0.1
 
         return action
 
